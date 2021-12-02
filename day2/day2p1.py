2d631e2cdde996d5a6f92975034a9fa350c67b85
Razvan Turcu <bluefowx@gmail.com>
2021-12-02 17:02:44 +0100

    day 2 pog

---
+++ day2/day2p1.py
@@ -0,0 +1,12 @@
+with open('input', 'r') as file:
+    inplist = file.read().split()
+
+pos = 0
+depth = 0
+
+for i in range(len(inplist)):
+    if inplist[i] == "forward": pos += int(inplist[i+1])
+    elif inplist[i] == "up": depth -= int(inplist[i+1])
+    elif inplist[i] == "down": depth += int(inplist[i+1])
+
+print(pos*depth)
---
+++ day2/day2p2.py
@@ -0,0 +1,15 @@
+with open('input', 'r') as file:
+    inplist = file.read().split()
+
+pos = 0
+depth = 0
+aim = 0
+
+for i in range(len(inplist)):
+    if inplist[i] == "up": aim -= int(inplist[i+1])
+    elif inplist[i] == "down": aim += int(inplist[i+1])
+    elif inplist[i] == "forward":
+        pos += int(inplist[i+1])
+        depth += aim * int(inplist[i+1])
+
+print(pos*depth)
