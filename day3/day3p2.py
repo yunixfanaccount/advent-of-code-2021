2066d29fc2fd98e87c114f7523ce657201e0a5ee
Razvan Turcu <bluefowx@gmail.com>
2021-12-03 23:57:44 +0100

    removed temp variables

--- day3/day3p2.py
+++ day3/day3p2.py
@@ -15,42 +15,28 @@ for x in range(1,13):
     ### OXY
     if len(oxyL) >= 2:
         if ones > (len(oxyL) - ones) or ones == (len(oxyL) - ones):
-            #print("elif:", ones) ### temp
             oxyLRM = [oxyL[i] for i in range(len(oxyL)) if int(oxyL[i][x-1:x]) != 1]
-            #print(oxyL) ### temp
             for i in oxyLRM:
                 oxyL.remove(i)
             
         else:
-            #print("else:", ones) ### temp
             oxyLRM = [oxyL[i] for i in range(len(oxyL)) if int(oxyL[i][x-1:x]) != 0]
-            #print(oxyL) ### temp
             for i in oxyLRM:
                 oxyL.remove(i)
-                #print("remove:", oxyLRM) ### temp
 
     ### CAR
     if len(carL) >= 2:
         if ones_car > (len(carL) - ones_car) or ones_car == (len(carL) - ones_car):
-            print("nb of 1:", ones_car) ### temp
             carLRM = [carL[i] for i in range(len(carL)) if int(carL[i][x-1:x]) != 0]
-            print(carL) ### temp
             for i in carLRM:
                 carL.remove(i)
             
         else:
-            print("else:", ones_car) ### temp
             carLRM = [carL[i] for i in range(len(carL)) if int(carL[i][x-1:x]) != 1]
-            print(carL) ### temp
             for i in carLRM:
                 carL.remove(i)
-                print("remove:", carLRM) ### temp
-    
-
 
 print(oxyL)
 print(carL)
-
 print(int(oxyL[0], 2), int(carL[0], 2))
-
 print(int(oxyL[0], 2) * int(carL[0], 2))
